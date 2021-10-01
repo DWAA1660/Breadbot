a6d8bdfd928ce411b74158f5bf79f22a02c4cbdd
DWAA1660 <80738438+DWAA1660@users.noreply.github.com>
2021-10-01 13:58:29 -0600

    Add files via upload

---
+++ cogs/files.py
@@ -0,0 +1,63 @@
+from os import write
+from nextcord.ext import tasks
+from asyncio.tasks import wait_for
+from sys import prefix
+import nextcord
+from nextcord import *
+from nextcord import channel
+from nextcord import reaction
+from nextcord import message
+from nextcord import emoji
+from nextcord import guild
+from nextcord.ext import commands
+import random
+import requests
+import asyncio
+import datetime
+import time
+
+class files(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+
+    
+
+    @commands.command(name="qotdwrite", brief="Adds a qotd to que")
+    async def qotdwrite(self, context: commands.Context, *, message):
+        with open('qotds.txt', 'a') as f:
+            f.write(message + '\n')
+
+    
+    
+    
+    @commands.command(name="qotd", brief="Adds a qotd to que")
+    async def qotdread(self, ctx):
+        with open('qotdnumber.txt', 'r+') as f:
+            readvariable = int(f.readline())
+        
+        file = open('qotds.txt')
+        all_lines = file.readlines()
+        oknow = (all_lines[readvariable])
+        await ctx.send(oknow)
+        readvariable += 1
+
+        str(readvariable)
+        with open('qotdnumber.txt', 'r+') as f:
+            f.write(str(readvariable))       
+      
+            
+         
+
+#@tasks.loop(seconds = 10 )
+#async def garf():
+ #   global num
+  #  channel = client.get_channel(873789547811602503)
+  #  await channel.send(file=discord.File(f'{num}.png'))
+  #  await channel.send(file=nextcord.File(fr'C:\Users\dwatn\OneDrive\Desktop\garfffi\Breadbot\{num}.png'))
+ #   num += 1
+
+
+def setup(client):
+    client.add_cog(files(client))
